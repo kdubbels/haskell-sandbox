348f1fd636ae399665fb8a4edd7b908aa43f30ba
kdubbels <dubbk381@alumni.newschool.edu>
2015-08-20 01:06:43 -0400

    successful fizzbuzz, motherfucker :sailboat:

--- baby.hs
+++ baby.hs
@@ -4,4 +4,8 @@ doubleUs x y = doubleMe x + doubleMe y
 
 doubleSmallNumber x = if x > 100  
                         then x  
-                        else x*2   
+                        else x*2
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+fizzBuzz xs = [ if x `mod` 3 == 0 then "FIZZ!" else if x `mod` 5 == 0 then "BUZZ!" else show x | x <- xs]
