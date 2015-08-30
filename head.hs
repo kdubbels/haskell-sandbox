1263b57d300eb3e3b8ca3005a42646cf47b9b406
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2015-08-29 22:40:07 -0400

    head

---
+++ head.hs
@@ -0,0 +1,16 @@
+head' :: [a] -> a  
+head' [] = error "Can't call head on an empty list, dummy!"  
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String  
+tell [] = "The list is empty"  
+tell (x:[]) = "The list has one element: " ++ show x  
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y  
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+badAdd :: (Num a) => [a] -> a
+badAdd (x:y:z:[]) = x + y + z
+
+firstLetter :: String -> String
+firstLetter "" = "Empty string, whoops!"
+firstLetter all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
