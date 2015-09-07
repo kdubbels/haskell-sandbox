39733793f374ebd17f3402ced8e57140882f24e5
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2015-09-07 01:23:04 -0400

    higher order functions

---
+++ higher-order.hs
@@ -0,0 +1,11 @@
+multThree :: Int -> Int -> Int -> Int
+multThree x y z = x * y * z
+
+compareWithHundred :: Int -> Ordering
+compareWithHundred = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
