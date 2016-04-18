30d84c89eb53b7299e7d0cf06ce0a680f8cb9492
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2016-04-17 21:34:15 -0400

    stuff

--- higher-order.hs
+++ higher-order.hs
@@ -9,3 +9,8 @@ divideByTen = (/10)
 
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]  
+zipWith' _ [] _ = []  
+zipWith' _ _ [] = []  
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys 
