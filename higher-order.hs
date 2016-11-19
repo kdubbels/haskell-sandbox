fbd82b6b0f6088122b9a58943769685a99e32926
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2016-11-19 00:57:10 -0700

    some higher order functions

--- higher-order.hs
+++ higher-order.hs
@@ -9,3 +9,19 @@ divideByTen = (/10)
 
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x = x:filter p xs
+    | otherwise = filter p xs
+
+chain :: Integer -> [Integer]
+chain 1 = [1]
+chain n
+    | even n = n:chain (n `div` 2)
+    | odd n = n:chain (n*3 + 1)
